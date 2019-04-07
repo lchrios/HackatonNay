12b9eb3acf9adcb47306312f6526d162707f168c
Christopher Ortega <A01634500@itesm.mx>
2019-04-06 23:34:16 -0600

    lastfix42day

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ function search() {
 function submitContactPetition() {
 
 }
+
+function createNewUser(){
+    let email = document.getElementById("inputMail").value;
+    let password = document.getElementById("inputPassword").value;
+    let rePassword = document.getElementById("inputRePassword").value;
+    if (password === rePassword) {
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
+    } else {
+        document.getElementById("inputPassword").style.borderColor = "#ff0300";
+        document.getElementById("inputRePassword").style.borderColor = "#ff0300";
+    }
+}
